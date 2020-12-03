4f5b6de2617aadeda4f0eebfb41e00636d1b5c61
KKe-coder <carumag726@gmail.com>
2020-12-03 03:07:06 +0000

    [Add] relationshipsコントローラ追加、内部記載

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,12 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    # redirect_to request.referer
+  end
+
+  def destroy
+    # redirect_to request.referer
+  end
+
 end
