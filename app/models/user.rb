9f554771984dde6e395d9c76aaeb7ce5aa56e460
KKe-coder <carumag726@gmail.com>
2020-12-03 03:29:00 +0000

    [Add] relationshipモデル・テーブルの作成と関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
+	has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :followerlist, through: :follower, source: :relationship
+	has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+	has_many :followedlist, through: :followed, source: :relationship
   attachment :profile_image, destroy: false
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
