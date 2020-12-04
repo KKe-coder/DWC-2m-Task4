130e22641f13978a2757c90608fe0b17a66029a7
KKe-coder <carumag726@gmail.com>
2020-12-04 02:46:24 +0000

    [Add]user/indexへのフォロワー機能追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,11 +2,15 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # redirect_to request.referer
+    follow = current_user.act_follower.build(followed_id: params[:user_id])
+    follow.save
+    redirect_to users_path
   end
 
   def destroy
-    # redirect_to request.referer
+    follow = current_user.act_follower.find_by(followed_id: params[:user_id])
+    follow.destroy
+    redirect_to users_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,22 @@ class User < ApplicationRecord
   has_many :books
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
-	has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-	has_many :followerlist, through: :follower, source: :relationship
-	has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-	has_many :followedlist, through: :followed, source: :relationship
+
+	has_many :act_follower, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+
+	has_many :followerlist, through: :act_follower, source: :followed
+
+	has_many :pas_followed, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
+
+	has_many :followedlist, through: :pas_followed, source: :follower
+
   attachment :profile_image, destroy: false
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
+    def followed_by?(user)
+      pas_followed.find_by(follower_id: user.id).present?
+    end
+
 end
