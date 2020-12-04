434c78ee31d279ac0d3ee7f0ca8ac73ea61660f7
KKe-coder <carumag726@gmail.com>
2020-12-04 03:13:25 +0000

    [Add]user_infoにおけるフォローする/解除ボタンの追加、動作時リダイレクト先の変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,13 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.act_follower.build(followed_id: params[:user_id])
     follow.save
-    redirect_to users_path
+    redirect_to request.referer
   end
 
   def destroy
     follow = current_user.act_follower.find_by(followed_id: params[:user_id])
     follow.destroy
-    redirect_to users_path
+    redirect_to request.referer
   end
 
 end
